a9371ac97c4663ec610cbd591909ac811f8b6fff
BEOMS <jbs980612@gmail.com>
2023-07-19 21:49:33 +0900

    Controller 생성, MyBatis연결 연습끝

---
+++ ex02/src/main/java/com/example/ex02/controller/ExampleController.java
@@ -0,0 +1,9 @@
+package com.example.ex02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex/*")
+public class ExampleController {
+}
