32fc19d70442d6cc413b1b55daa89d1c50fa7d01
BEOMS <jbs980612@gmail.com>
2023-07-21 23:25:32 +0900

    Controller 공부2

--- ex02/src/main/java/com/example/ex02/controller/ExampleController.java
+++ ex02/src/main/java/com/example/ex02/controller/ExampleController.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.example.ex02.domain.MemberVO;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import java.util.List;
+
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -42,5 +48,28 @@ public class ExampleController {
     }
     @GetMapping("ex07")
     public void ex07(){
+    }
+    @GetMapping("ex08")
+    public String  ex08(){
+        return "ex/ex08/ex08";
+    }
+    @GetMapping("ex09")
+    public String ex09(String name, Model model/*, HttpServletRequest request*/){
+//        request.setAttribute("name", name);
+        model.addAttribute("name", name);
+        return "ex/ex09";
+    }
+    @GetMapping("ex10")
+    public String ex10(MemberVO memberVO){
+        return "ex10";
+    }
+    @GetMapping("ex11")
+    public void ex11(MemberVO memberVO, @ModelAttribute("gender") String gender){
+
+    }
+    @GetMapping("ex12")
+    public void ex12(@RequestParam("data") List<String> datas) {
+        log.info("=================================");
+        datas.forEach(log::info);
     }
 }
