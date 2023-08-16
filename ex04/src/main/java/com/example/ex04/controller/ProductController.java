e6f4e13c86aca6d789b69074aaaa891e3642dec3
BEOMS <jbs980612@gmail.com>
2023-08-16 18:11:06 +0900

    REST 예제3-2

---
+++ ex04/src/main/java/com/example/ex04/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.ex04.controller;
+
+import com.example.ex04.domain.ProductVO;
+import com.example.ex04.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/product/*")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("list")
+    public String list(Model model){
+        model.addAttribute("productForm", new ProductVO());
+        model.addAttribute("products", productService.getList());
+        return "/product";
+    }
+    @PostMapping("new")
+    @ResponseBody
+    public void register(@RequestBody ProductVO productVO){
+        productService.register(productVO);
+    }
+}
